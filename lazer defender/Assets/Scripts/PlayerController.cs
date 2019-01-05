5bd00576cb6e3bd582c10a558384da589da7d849
d1o1m133 <d.addonizio@yahoo.com>
2019-01-05 17:10:00 -0500

    Uploading project

---
+++ lazer defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float mouseMoveSpeed = 1000f;
+    private Vector3 mousePosition;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    public GameObject laserPrefab;
+    private float projectileSpeed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    
+    void Update()
+    {
+        Fire();
+        Move();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+                GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mousePosition - transform.position).normalized;
+            float newX = direction.x * mouseMoveSpeed * Time.deltaTime;
+            float newY = direction.y * mouseMoveSpeed * Time.deltaTime;
+            
+            rb.velocity = new Vector2(newX, newY);
+           
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
+
---
+++ lazer defender/lazer defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float mouseMoveSpeed = 1000f;
+    private Vector3 mousePosition;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    
+    void Update()
+    {Move();}
+    
+    private void Move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mousePosition - transform.position).normalized;
+            float newX = direction.x * mouseMoveSpeed * Time.deltaTime;
+            float newY = direction.y * mouseMoveSpeed * Time.deltaTime;
+            
+            rb.velocity = new Vector2(newX, newY);
+           
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
+
