d1408f7b76ba34bef330048ca5f5ede47bd1f142
d1o1m133 <d.addonizio@yahoo.com>
2019-01-14 11:14:38 -0500

    Changes to player
    I made it so the player shoots form a point above him, that way the bullets dont collide with him.
    
    i also made it so if your hold down space bar the player will shoot automatically

--- lazer defender/Assets/Scripts/PlayerController.cs
+++ lazer defender/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform FirePoint;
     private float mouseMoveSpeed = 1000f;
     private Vector3 mousePosition;
     private Vector2 direction;
     private Rigidbody2D rb;
     public GameObject laserPrefab;
     private float projectileSpeed = 10f;
+    private float projectileFiringPeriod = 0.1f;
+    Coroutine firingCoroutine;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+       
     }
 
-    
+
     void Update()
     {
         Fire();
@@ -29,9 +32,12 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
+           firingCoroutine = StartCoroutine(FireFast());
             
-                GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
-                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
         }
     }
 
@@ -39,19 +45,36 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            
+
             mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             direction = (mousePosition - transform.position).normalized;
             float newX = direction.x * mouseMoveSpeed * Time.deltaTime;
             float newY = direction.y * mouseMoveSpeed * Time.deltaTime;
-            
+
             rb.velocity = new Vector2(newX, newY);
-           
+
         }
         else
         {
             rb.velocity = Vector2.zero;
         }
     }
+  IEnumerator FireFast()
+    {
+        while (true)
+        {
+            GameObject laser = Instantiate(laserPrefab, FirePoint.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
+
+
+    }
 }
+
+
+   
+
+
 
